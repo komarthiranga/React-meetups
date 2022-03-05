0cf7238c4d5e534e16e9f8daf67f20f8e05d3f9c
Ranga Komarthi <88224031+komarthiranga@users.noreply.github.com>
2022-03-05 13:13:58 +0530

    build issue has been resloved for new page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -29,7 +29,7 @@ export async function getStaticPaths() {
   const meetups = await collection.find({}, { _id: 1 }).toArray();
   client.close();
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
